d9884766550c294cade95c03811b7a2aaf021a8a
Meendo <meenphilip@gmail.com>
2024-08-18 22:20:45 +0300

    Initial project setup

---
+++ courses/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Subject Model
+class Subject(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+# Course Model
+class Course(models.Model):
+    owner = models.ForeignKey(
+        User, related_name="courses_created", on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject, related_name="courses", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+
+# Module models
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
