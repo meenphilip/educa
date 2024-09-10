914a9b9b4cb2ff7f0baee73d1643ea055a0fba10
Meendo <meenphilip@gmail.com>
2024-09-10 22:47:02 +0300

    Enrolling on courses

--- courses/models.py
+++ courses/models.py
@@ -25,6 +25,7 @@ class Course(models.Model):
     subject = models.ForeignKey(
         Subject, related_name="courses", on_delete=models.CASCADE
     )
+    students = models.ManyToManyField(User, related_name="courses_joined", blank=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     overview = models.TextField()
